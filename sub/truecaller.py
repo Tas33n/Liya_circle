19c34f758ac6cba492d06d31f4e2d66582964cdb
Farhan Isteak Taseen <55633040+Tas33n@users.noreply.github.com>
2022-06-05 10:55:25 +0600

    Add files via upload

--- sub/truecaller.py
+++ sub/truecaller.py
@@ -3,23 +3,24 @@ import  requests
 #headers required and bearer is authorization token
 
 head={
-    "Host": "search5-noneu.truecaller.com",
-    "authorization": "Bearer a1i0Q--YLbhyeF9-Wrv86LslFomuMonYQBfGiV5X-ma5m_EXKgSZPWn_R6K0SJaS",
-    "accept-encoding": "gzip",
-    "user-agent": "Truecaller/11.66.7 (Android;10)"
+    
  } 
   
 def main(num):
 
-    url='https://search5-noneu.truecaller.com/v2/search?countryCode=BD&type=4&locAddr=&placement=SEARCHRESULTS%2CHISTORY%2CDETAILS&adId=&encoding=json&q='+str(num)
+    url='https://redthryssa.xyz/numberlookup.php?id='+str(num)
     req=requests.get(url,headers=head)
     data=req.json()
     if data:
-        name=data['data'][0]['name'] if data['data'] else None                
+        name=data['data'][0]['name'] if data['data'] else None       
+        gender=data['data'][0]['gender'] if data['data'] else None                   
         carrier=data['data'][0]['phones'][0]['carrier']  if data['data'][0]['phones'] else None
+        phone=data['data'][0]['phones'][0]['e164Format']  if data['data'][0]['phones'] else None
         email=data['data'][0]['internetAddresses'][0]['id']  if data['data'][0]['internetAddresses'] else None
-        address=data['data'][0]['addresses'][0]['city']  if data['data'][0]['addresses'] else None
-        info = [name,carrier,email,address]
+        address=data['data'][0]['addresses'][0]['address']  if data['data'][0]['addresses'] else None
+        img=data['data'][0]['image'] if data['data'] else None   
+        info = [img,"User infos",name,phone,carrier,email,address]
+       # info = ("name: ", name, )
         info = list(filter(None,info))
 
     
